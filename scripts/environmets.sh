4035800905fc06b8f64f562155bb975d97e5fe8a
airdata <airdata@abv.bg>
2019-12-07 19:19:18 +0200

    add win

--- scripts/environmets.sh
+++ scripts/environmets.sh
@@ -7,31 +7,13 @@ ZBX_PUBLIC_IP=$(ip route get 1 | awk '{print $NF;exit}')
 GRF_SERVER_URL="https://admin:zabbix@localhost:3000"
 yesPattern="^[Yy][Ee][Ss]"
 HOST_GROUPS=(
-"BSD servers" \
-"Windows servers" \
-"Firewalls" \
-"Routers" \
-"Switches" \
-"Netscalers"
-"Nginx servers" \
-"Apache servers" \
-"Litespeed servers" \
-"Haproxy servers" \
 "Tomcat servers" \
 "NodeJS servers" \
 "JVM servers" \
-"IIS servers" \
 "MySQL servers" \
-"PostgreSQL servers" \
-"MongoDB servers" \
 "Oracle servers" \
-"MSSQL servers" \
 "RabbitMQ servers" \
-"Couchbase servers" \
-"Redis servers" \
 "Kafka servers" \
 "Docker servers" \
 "Kubernetes servers" \
-"Openshift servers" \
-"Mesos servers"
 )

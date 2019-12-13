c0e5bafdd4de385de79902ddb857f05b946c081f
airdata <airdata@abv.bg>
2019-12-13 20:29:17 +0200

    add

--- scripts/environmets.sh
+++ scripts/environmets.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #Environments
-ZBX_SERVER_URL="https://localhost:8443"
+ZBX_SERVER_URL="http://localhost:8081"
 ZBX_PUBLIC_IP=$(ip route get 1 | awk '{print $NF;exit}')
-GRF_SERVER_URL="https://admin:zabbix@localhost:3000"
+GRF_SERVER_URL="http://admin:zabbix@localhost:3000"
 yesPattern="^[Yy][Ee][Ss]"
 HOST_GROUPS=(
 "Tomcat servers" \
